b2db0e099a3d292e26c717fffa087dccac3dfb44
Neldrine <neldrine@gmail.com>
2024-04-24 10:10:17 +0100

    added endpoint urls to the api routes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/rover', [\App\Http\Controllers\RoverPhotoController::class, 'index'])
     ->middleware(\App\Http\Middleware\IsUserAuthenticated::class);
 
+// pass email + password to get a token
+Route::get('/authenticate', [\App\Http\Controllers\AuthController::class, 'createToken']);
+
+//use this endpoint to retrieve user details
+Route::get('/token', [\App\Http\Controllers\AuthController::class, 'readToken']);
+
+//use this endpoint to refresh the token
+Route::post('/token', [\App\Http\Controllers\AuthController::class, 'updateToken']);
+
+// use this endpoint to destroy/log out
+Route::delete('/token', [\App\Http\Controllers\AuthController::class, 'deleteToken']);
+
